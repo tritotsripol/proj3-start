e2d5baf5a2c65d939f7be20ec696283746446797
tritotsripol <tritotsripol@gmail.com>
2025-04-01 16:54:01 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+const gotop = document.querySelector('.gotop');
+gotop.addEventListener('click',function() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+});
+
+function changeLanguage(language) {
+  document.querySelectorAll('[data-' + language + ']').forEach(function(element) {
+    element.textContent = element.getAttribute('data-' + language);
+  });
+}
+changeLanguage('en');
+
+// theme
+const themeToggleButton = document.getElementById('theme-toggle');
+const themeIcon = document.getElementById('theme-icon');
+const prismTheme = document.getElementById('prism-theme');
+
+// ตรวจสอบธีมที่บันทึกไว้
+let savedTheme = localStorage.getItem('theme');
+
+if (savedTheme === 'light') {
+  document.body.classList.add('light');
+  document.body.classList.remove('dark');
+  themeIcon.src = 'sun-icon.png'; // ไอคอนพระอาทิตย์ (สว่าง)
+  prismTheme.href = 'https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/themes/prism.min.css'; // ใช้ธีม Prism แบบสว่าง
+} else {
+  document.body.classList.add('dark');
+  document.body.classList.remove('light');
+  themeIcon.src = 'moon-icon.png'; // ไอคอนพระจันทร์ (มืด)
+  prismTheme.href = 'https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/themes/prism-tomorrow.min.css'; // ใช้ธีม Prism แบบมืด
+}
+
+// ฟังก์ชันเปลี่ยนธีม
+themeToggleButton.addEventListener('click', () => {
+  const isLight = document.body.classList.toggle('light');
+  document.body.classList.toggle('dark', !isLight);
+
+  if (isLight) {
+    localStorage.setItem('theme', 'light');
+    themeIcon.src = 'sun-icon.png'; 
+    prismTheme.href = 'https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/themes/prism.min.css'; 
+  } else {
+    localStorage.setItem('theme', 'dark');
+    themeIcon.src = 'moon-icon.png'; 
+    prismTheme.href = 'https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/themes/prism-tomorrow.min.css'; 
+  }
+});
